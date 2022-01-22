090566d66e88314e8a3e279fe9c6212019236b8f
fraing31 <cisco120988@gmail.com>
2022-01-21 22:41:37 -0500

    Patio de juegos

--- server.py
+++ server.py
@@ -2,21 +2,28 @@ from flask import Flask, render_template
 
 app = Flask( __name__ )
 
-@app.route('/')
-def index():
-    return render_template("index.html")
 
-@app.route('/play')
-def index():
-    return render_template("index.html")
+@app.route('/play', methods=['GET'])
+def play():
 
+    return render_template( "hello.html" ) 
 
-@app.route( '/hello/<string:banana>/<int:num>', methods=['GET'])
+@app.route('/play/<int:num>', methods=['GET'])
+def playDiv( num ):
 
-def hello(banana, num):
+    return render_template( "number.html", num=num )
 
-    return render_template("hello.html", banana=banana, num=num )
+@app.route('/play/<int:num>/<colorBox>', methods=['GET'])
+def playDivColor( num, colorBox):
 
+    return render_template( "index.html", num=num, colorBox=colorBox )
 
 if __name__ == "__main__":
     app.run( debug=True )
+
+
+#@app.route( '/hello/<string:banana>/<int:num>', methods=['GET'])##
+
+#def hello(banana, num):##
+
+#   return render_template("hello.html", banana=banana, num=num )##
